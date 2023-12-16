b5cfe06e48bb8388eb4d1849d9f0b81a432a05b3
Abhishek Maurya <abhim0505@gmail.com>
2023-12-16 14:58:36 +0530

    Context updated

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHouse,
@@ -8,8 +8,8 @@ import {
 import { RecipeContext } from "../context/RecipeContext";
 
 const Header = () => {
-  const { search, setSearch, apiData } = useContext(RecipeContext);
-
+  const { apiData, recipes } = useContext(RecipeContext);
+  const [search, setSearch] = useState("");
   const handleInputChange = (e) => {
     setSearch(e.target.value);
   };
@@ -18,29 +18,44 @@ const Header = () => {
     apiData(search);
   };
 
+  useEffect(() => {
+    console.log(recipes)
+  }, [])
+
   return (
-    <div className="w-full h-16  flex justify-evenly items-center border-b-2">
-      <span className="cursor-pointer">
-        <FontAwesomeIcon icon={faHouse} />
-      </span>
-      <div className="flex justify-between items-center w-1/4 ring-1 ring-slate-600 rounded-md p-2">
-        <input
-          type="text"
-          className="outline-none w-full"
-          placeholder="Enter Recipe Name"
-          value={search}
-          onChange={handleInputChange}
-        />
-        <FontAwesomeIcon
-          icon={faMagnifyingGlass}
-          className="cursor-pointer"
-          onClick={handleButtonClick} // Trigger search on icon click
-        />
+    <>
+      <div className="w-full h-16  flex justify-evenly items-center border-b-2">
+        <span className="cursor-pointer">
+          <FontAwesomeIcon icon={faHouse} />
+        </span>
+        <div className="flex justify-between items-center w-1/4 ring-1 ring-slate-600 rounded-md p-2">
+          <input
+            type="text"
+            className="outline-none w-full"
+            placeholder="Enter Recipe Name"
+            value={search}
+            onChange={handleInputChange}
+          />
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="cursor-pointer"
+            onClick={handleButtonClick} // Trigger search on icon click
+          />
+        </div>
+        <span className="cursor-pointer">
+          <FontAwesomeIcon icon={faHeart} />
+        </span>
       </div>
-      <span className="cursor-pointer">
-        <FontAwesomeIcon icon={faHeart} />
-      </span>
-    </div>
+      <div className="w-4/5  m-auto ">
+        <ul className="flex ">
+          {recipes.map((recipe) => {
+            <li key={recipe.id}>
+              <h2>{recipe.title}</h2>
+            </li>;
+          })}
+        </ul>
+      </div>
+    </>
   );
 };
 
